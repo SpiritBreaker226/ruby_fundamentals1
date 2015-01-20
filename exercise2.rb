b4b9b5681e1b42d7934502071bf233fd724852f1
Jason Stathopulos <jstathopulos@gmail.com>
2015-01-20 15:03:56 -0500

    Add in logical statement and guess correctly that it will display true

--- exercise2.rb
+++ exercise2.rb
@@ -3,3 +3,6 @@ amount = 55
 puts "A 15% tip of #{amount} is $" << (amount * 0.15).to_s
 
 puts "45628 * 7839 = #{45628 * 7839}"
+
+puts "This is will return true because !(false && false) is true and since it uses OR then "
+puts "The logic statment (true && false) || (false && true) || !(false && false) is " << ((true && false) || (false && true) || !(false && false)).to_s
